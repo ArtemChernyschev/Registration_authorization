612654b51d8ac64cab4ac57ff1426746e455b64a
Артем Чернышев <art@chernyschev.com>
2023-04-11 11:36:18 +0300

    code-review 10.4.23
    1.1) Почему txt файл на гите?
    	 	 1.2) Почему два py файла на гите? - убрал
    	 	 1.3) 46-47 строки лучше вынести в начало файла в виде констант; - перенес
    	 	 1.4) Должна быть возможность автоматически создать файл, если его нет на устройстве юзера;
    	 	 - создал функцию create_file
    	 	 1.5) Запрос данных лучше сделать в отдельной функции; - создал функцию intro_logpass
    	 	 1.6) Проверку также лучше сделать в отдельной функции; test_log(file_name), test_ pass(file_name)
    	 	 1.7) 25 строка лишняя; - удалил
    	 	 1.8) comp_data - странное название функции;  изменил на check_file

--- registration_authorization.py
+++ registration_authorization.py
@@ -1,63 +1,86 @@
-def write(login, password):
-    with open('storage.txt', 'a', encoding='utf-8') as f:
+reg = '1'
+auth = '2'
+
+
+def create_file(file_name):
+    try:
+        with open(file_name, 'x', encoding='utf-8') as f:
+            print(f'Файл {file_name} успешно создан')
+    except FileExistsError:
+        print(f'Файл {file_name} уже есть')
+
+
+def write_file(file_name, login, password):
+    with open(file_name, 'a', encoding='utf-8') as f:
         f.write(f'{login}:{password}\n')
         print('Пользователь успешно зарегистрирован')
 
 
-def comp_data(login):
-    with open('storage.txt', encoding='utf-8') as f:
+def check_file(file_name, login):
+    with open(file_name, encoding='utf-8') as f:
         for line in f:
             saved_login, saved_password = line.split(':')
             if login == saved_login:
                 return saved_password.strip()
 
-        
-def registration():
-    print('Регистрация нового пользователя')
+
+def intro_logpass(file_name):
+    login = input('Пожалуйста, введите логин: ')
+    password = input('Введите пароль: ')
+    return login, password
+
+
+def test_log(file_name):
+    login, password = intro_logpass(file_name)
     while True:
-        login = input('Пожалуйста, введите логин (от 3 до 20 символов): ')
         assert 3 <= len(login) <= 20, 'Некорректный логин'
-        password = input('Введите пароль: ')
         assert 4 <= len(password) <= 32, 'Некорректный пароль'
-        if comp_data(login):
+        if check_file(file_name, login):
             print('Пользователь уже зарегистрирован')
             break
-        else:
-            write(login, password)
-            break
+        write_file(file_name, login, password)
+        break
 
 
-def authorization():
-    print('Авторизация пользователя')
+def test_pass(file_name):
+    login, password = intro_logpass(file_name)
     while True:
-        login = input('Введите логин: ')
-        if not comp_data(login):
+        if not check_file(file_name, login):
             print('Такого пользователя не существует')
             break
-        password = input('Введите пароль: ')
-        saved_password = comp_data(login)
+        saved_password = check_file(file_name, login)
         if password == saved_password:
             print('Авторизация успешна')
             break
         print('Неправильный пароль')
 
 
-def input_data():
-    reg = '1'
-    auth = '2'
+def registration(file_name):
+    print('Регистрация нового пользователя')
+    test_log(file_name)
+
+
+def authorization(file_name):
+    print('Авторизация пользователя')
+    test_pass(file_name)
+
+
+def entry_data(file_name):
     while True:
         reg_or_auth = input("'1' - для регистрации, '2' - для авторизации: ")
         if reg_or_auth == reg:
-            registration()
+            registration(file_name)
         elif reg_or_auth == auth:
-            authorization()
+            authorization(file_name)
         else:
             print('Некорректный ввод')
 
 
 def main():
+    file_name = input('Введите имя файла для хранения: ')
+    create_file(file_name)
     print("Привет! Выбери дейстивие:")
-    input_data()
+    entry_data(file_name)
 
 
 while True:
